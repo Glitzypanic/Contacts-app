a530284ef879d346d5c1c9437e6dca1366719702
Jose Farias <jose.fariasdv@gmail.com>
2023-12-15 22:37:45 -0300

    Simulate database whit JSON File

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-  $contacts = [
-    ["name" => "Jose", "phone_number" => "+569 42435588"],
-    ["name" => "Tamara", "phone_number" => "+569 42435588"],
-    ["name" => "Abigail", "phone_number" => "+569 42435588"]
-  ]
+  if (file_exists("contacts.json")) {
+    $contacts = json_decode(file_get_contents("contacts.json"), true);
+  } else {
+    $contacts = [];
+  }
 
 ?>
 <!DOCTYPE html>
@@ -57,7 +57,7 @@
               <a class="nav-link" href="./index.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="./add.html">Add Contact</a>
+              <a class="nav-link" href="./add.php">Add Contact</a>
             </li>
           </ul>
         </div>
@@ -67,7 +67,15 @@
     <main>
       <div class="container pt-4 p-3">
         <div class="row">
-         <?php foreach($contacts as $contact) { ?>
+          <?php if (count($contacts) == 0): ?>
+            <div class="col-md-4 mx-auto">
+              <div class="card card-body text-center">
+                <p>No contacts saved yet</p>
+                <a href="add.php">Add One!</a>
+              </div>
+            </div>
+          <?php endif ?>
+          <?php foreach($contacts as $contact) { ?>
             <div class="col-md-4 mb-3">
               <div class="card text-center">
                 <div class="card-body">
